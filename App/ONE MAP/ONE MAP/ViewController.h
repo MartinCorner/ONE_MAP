de0439b36467532f5ca2555b233aadd62fb3f661
martin <appcre@live.com>
2015-06-11 15:14:50 +0800

    add app files

---
+++ App/ONE MAP/ONE MAP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ONE MAP
+//
+//  Created by martin on 15/6/11.
+//  Copyright (c) 2015年 martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ App/ONE MAP/ONE MAP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ONE MAP
+//
+//  Created by martin on 15/6/11.
+//  Copyright (c) 2015年 martin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
