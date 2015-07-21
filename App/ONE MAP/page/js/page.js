1db2620a8a614d34aa899e0d459a6dc78b8ae405
wenqing.wan <appcre@live.com>
2015-07-21 13:21:27 +0800

    update iOS App, add 'jvectormap_demo'

--- App/ONE MAP/page/js/page.js
+++
@@ -1,7 +0,0 @@
-$('.button-collapse').sideNav({
-      menuWidth: 240, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-Waves.init();
